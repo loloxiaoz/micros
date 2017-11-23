3b2bfd13afbe050b58ce51ef4bd178c2e6d45e65
luopan <luopan@w1.dev.aly.xcodecraft.cn>
2017-11-23 14:57:54 +0800

    add xbox

---
+++ src/orm/entity.go
@@ -0,0 +1,18 @@
+package orm
+
+//
+//import (
+//	"errors"
+//)
+//
+//type Entity struct {
+//	ID        uint `micros:"primary_key"`
+//	Ver       int  `sql:"version"`
+//	CreatedAt time.Time
+//	UpdatedAt time.Time
+//	DeletedAt time.Time
+//}
+//
+//func (e *Entity) Save() {
+//	e.db.Save(*e)
+//}
